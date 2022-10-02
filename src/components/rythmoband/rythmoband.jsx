1dc204c88c5d53bd4b6a1e53296e811a30aca560
ahmed ali <aniazi.bese20seecs@seecs.edu.pk>
2022-10-02 15:37:46 +0500

    ok

--- src/components/rythmoband/rythmoband.jsx
+++ src/components/rythmoband/rythmoband.jsx
@@ -41,7 +41,43 @@ export default function Rythmoband(props) {
     var total_seconds = hours * 3600 + minutes * 60 + seconds + millis / 1000;
     return total_seconds;
   }
+  function NewSRTDownload(dialogues, sub_time) {
+    var srt = "";
+    for (var i = 1; i <= sub_time.length; i++) {
+      var dial_number = `${i}`;
+      var time_s = `${sub_time[i - 1][0]}`;
+      time_s = time_s.trim();
+      time_s = setCharAt(time_s, 8, ",");
+      var time_e = `${sub_time[i - 1][1]}`;
+      time_e = time_e.trim();
+      time_e = setCharAt(time_e, 8, ",");
+      var dialogue = `${dialogues[i - 1]}`;
+      var cluster =
+        dial_number +
+        "\n" +
+        time_s +
+        " --> " +
+        time_e +
+        "\n" +
+        dialogue +
+        "\n" +
+        "" +
+        "\n";
+      srt = srt + cluster;
+    }
 
+    console.log(srt);
+    var data = "text;charset=utf-8,";
+    var a = document.createElement("a");
+    a.href = "data:text/plain," + encodeURI(srt);
+    a.target = "_blank";
+    a.download = "NEW_SRT.srt";
+    a.innerHTML = "";
+    var container = document.getElementById("container");
+    container.appendChild(a);
+    a.click();
+    container.removeChild(a);
+  }
   const [number, setnumber] = useState(
     props.dialogueNumber === undefined ? 0 : props.dialogueNumber
   );
